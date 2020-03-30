faea24fcd7827b698bd72ad5309d641ad8fd279e
ryanatgz <61959975+ryanatgz@users.noreply.github.com>
2020-03-30 21:57:35 +0800

    Update diy.sh
    add passwall to firmware

--- diy.sh
+++ diy.sh
@@ -6,4 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.188/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.188/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall
