5c5f83925098a49deb95036978da852c4c559bc7
ryanatgz <61959975+ryanatgz@users.noreply.github.com>
2020-03-30 21:59:34 +0800

    Update diy.sh
    update install

--- diy.sh
+++ diy.sh
@@ -8,3 +8,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.188/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall
+./scripts/feeds update -a
+./scripts/feeds install -a
